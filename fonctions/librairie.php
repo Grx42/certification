f88f3e0223adeb51050af11d7577232befc983cd
simplon-gregoryg <grexor@hotmail.com>
2016-08-08 14:54:57 +0200

    Ajout librairie de fonctions

---
+++ fonctions/librairie.php
@@ -0,0 +1,18 @@
+<?php
+
+    function cleanPost($user_input) //nettoie les donnees des forumulaires
+    {
+        $user_input = trim($user_input); // enleve les espace en debut et fin de string
+        $user_input = strip_tags($user_input); // enleve les tags html <>
+        $user_input = stripslashes($user_input); // enleve les caracteres d'echapement "\"
+        $user_input = str_replace("'", "", $user_input); // supprime les apostrophes pour eviter les injections sql
+        return $user_input;
+    }
+
+    function logError()
+    {
+        echo "Erreur de mot de passe. <br>";
+        session_destroy();
+        echo "<a href='index.php'>Retour a l'acceuil</a>";
+    }
+?>
