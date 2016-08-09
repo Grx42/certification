5c8fc418870ae09cbceb2602e9d97b534c24aa1f
simplon-gregoryg <grexor@hotmail.com>
2016-08-09 22:10:14 +0200

    cleanPost echape les apostrophes et guillemets au lieu de les supprimer

--- fonctions/librairie.php
+++ fonctions/librairie.php
@@ -5,14 +5,14 @@
         $user_input = trim($user_input); // enleve les espace en debut et fin de string
         $user_input = strip_tags($user_input); // enleve les tags html <>
         $user_input = stripslashes($user_input); // enleve les caracteres d'echapement "\"
-        $user_input = str_replace("'", "", $user_input); // supprime les apostrophes pour eviter les injections sql
-        $user_input = str_replace('"', '', $user_input); // supprime les guillemets pour eviter les injections sql
+        $user_input = str_replace("'", "\'", $user_input); // echape les apostrophes pour eviter les injections sql
+        $user_input = str_replace('"', '\"', $user_input); // echape les guillemets pour eviter les injections sql
         return $user_input;
     }
 
     function logError()
     {
-        echo "Erreur. Compte ou mot de passe incorrect. <br>";
+        echo "<p>Erreur. Compte ou mot de passe incorrect.</p><br>";
         session_destroy();
         echo "<a href='index.php'>Retour a l'acceuil</a>";
     }
