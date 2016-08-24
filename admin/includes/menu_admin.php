d49a4f5e56ff488abc59e5fcf6bc1aa448abc761
simplon-gregoryg <grexor@hotmail.com>
2016-08-24 19:24:38 +0200

    Add lien pour retourner au site dans le menu admin

--- admin/includes/menu_admin.php
+++ admin/includes/menu_admin.php
@@ -5,5 +5,6 @@
         <li><a href="ecrire_article.php">Ecrire un article</a></li>
         <li><a href="lire_publies.php">Consulter les articles</a></li>
         <li><a href="lire_brouillon.php">Consulter les brouillons</a></li>
+        <li><a href="../index.php">Revenir au site</a></li>
     </ul>
 </nav>
