e1fbc5707caa65ffedd93594d3e8a6cf56f2ac6b
simplon-gregoryg <grexor@hotmail.com>
2016-08-25 16:17:33 +0200

    Add lecture contact en zone admin + maj menu admin

--- admin/includes/menu_admin.php
+++ admin/includes/menu_admin.php
@@ -5,6 +5,7 @@
         <li><a href="ecrire_article.php">Ecrire un article</a></li>
         <li><a href="lire_publies.php">Consulter les articles</a></li>
         <li><a href="lire_brouillon.php">Consulter les brouillons</a></li>
+        <li><a href="lire_contact.php">Consulter les messages de contact</a></li>
         <li><a href="../index.php">Revenir au site</a></li>
     </ul>
 </nav>
