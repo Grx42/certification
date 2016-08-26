45595a1428193a9b91f13c8c07473d7159618926
simplon-gregoryg <grexor@hotmail.com>
2016-08-26 17:45:23 +0200

    Add articles depuis la bdd + nouvelle bdd

--- includes/articles.php
+++ includes/articles.php
@@ -9,63 +9,54 @@
             </div>
         </div>
 
+        <?php
+            $req = $bdd->prepare('SELECT idArticles, titre, contenu, date_redac, login FROM articles LEFT JOIN comptes ON comptes_idcomptes = idcomptes WHERE brouillon = :est_un_brouillon ORDER BY date_redac DESC');
+
+            $req->execute(array(
+                'est_un_brouillon' => 0
+            ));
+
+
+            while($articles = $req->fetch())
+            {
+        ?>
+
         <div class="row">
-
             <div class="col-sm-4">
                 <div class="single-blog">
-                    <img src="images/blog/GroupeSimplonIness.jpg" alt="Groupe Simplon Iness" />
-                    <h3>Titre 1</h3>
+                    <h3><?php echo $articles['titre']; ?></h3>
                     <ul class="post-meta">
-                        <li><i class="fa fa-pencil-square-o"></i><strong> Posted By:</strong> John</li>
-                        <li><i class="fa fa-clock-o"></i><strong> Posted On:</strong> Apr 15 2014</li>
+                        <li><i class="fa fa-pencil-square-o"></i><strong> Posted By:</strong> <?php echo $articles['login']; ?></li>
+                        <li><i class="fa fa-clock-o"></i><strong> Posted On:</strong><?php echo $articles['date_redac']; ?></li>
                     </ul>
                     <div class="blog-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        <p><?php echo $articles['contenu']; ?></p>
                     </div>
-                    <a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-detail">Read More</a>
+                    <a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-detail<?php echo $articles['idArticles']; ?>">Read More</a>
                 </div>
-                <div class="modal fade" id="blog-detail" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal fade" id="blog-detail<?php echo $articles['idArticles']; ?>" tabindex="-1" role="dialog" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-body">
                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                <h2>Sous-Titre 1</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                <h2><?php echo $articles['titre']; ?></h2>
+                                <p><?php echo $articles['contenu']; ?></p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        <?php
+        }
 
+        $req->closeCursor();
+
+        ?>
+
+
+            <?php /*
             <div class="col-sm-4">
                 <div class="single-blog">
-                    <img src="images/blog/Simplon_HourOfCode.jpg" alt="Simplon Hour Of Code" />
-                    <h3>Titre 2</h3>
-                    <ul class="post-meta">
-                        <li><i class="fa fa-pencil-square-o"></i><strong> Posted By:</strong> John</li>
-                        <li><i class="fa fa-clock-o"></i><strong> Posted On:</strong> Apr 15 2014</li>
-                    </ul>
-                    <div class="blog-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                    </div>
-                    <a href="" class="btn btn-primary" data-toggle="modal" data-target="#blog-two">Read More</a>
-                </div>
-                <div class="modal fade" id="blog-two" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-body">
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                <h2>Sous-Titre 2</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-                <div class="single-blog">
-                    <img src="images/blog/Finformation.jpg" alt="Fin Formation.jpg" />
                     <h3>Titre 3</h3>
                     <ul class="post-meta">
                         <li><i class="fa fa-pencil-square-o"></i><strong> Posted By:</strong> John</li>
@@ -88,7 +79,7 @@
                     </div>
                 </div>
             </div>
-
+             */ ?>
 
         </div>
     </div>
