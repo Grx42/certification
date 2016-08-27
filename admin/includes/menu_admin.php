af901ea5955086225318451d002f07025a668f09
simplon-gregoryg <grexor@hotmail.com>
2016-08-27 17:34:37 +0200

    Zone admin : retour du menu admin

--- admin/includes/menu_admin.php
+++ admin/includes/menu_admin.php
@@ -1,12 +1,22 @@
-<nav>
-    <?php //menu vertical ?>
-
-    <ul>
-        <li><a href="ecrire_article.php">Ecrire un article</a></li>
-        <li><a href="lire_publies.php">Consulter les articles</a></li>
-        <li><a href="lire_brouillon.php">Consulter les brouillons</a></li>
-        <li><a href="lire_contact.php">Consulter les messages de contact</a></li>
-        <li><a href="profil.php">Editer le profil</a></li>
-        <li><a href="../index.php">Revenir au site</a></li>
-    </ul>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span>Le Blog de la Certif</span> : Zone Administrateur</a>
+            <ul class="user-menu">
+                <li class="dropdown pull-right">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg><?php echo $_SESSION['login']; ?><span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="profil.php"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Editer le profil</a></li>
+                        <li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div><!-- /.container-fluid -->
 </nav>
--- admin/profil.php
+++ admin/profil.php
@@ -8,30 +8,7 @@
     <?php include_once('../../../pdo_blog.php'); //connexion a la bdd) ?>
     <?php include_once("../fonctions/librairie.php"); ?>
     <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#"><span>Le Blog de la Certif</span> Admin</a>
-                    <ul class="user-menu">
-                        <li class="dropdown pull-right">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> User <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="profil.php"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
-                                <li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Settings</a></li>
-                                <li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-
-            </div><!-- /.container-fluid -->
-        </nav>
+        <?php include_once('includes/menu_admin.php'); ?>
 
         <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
 
