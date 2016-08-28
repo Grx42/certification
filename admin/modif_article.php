9780c97318e794e6a862c749944cbf4135c40b04
simplon-gregoryg <grexor@hotmail.com>
2016-08-28 16:23:30 +0200

    modif_article fix parametre manquant au retour sur la page

--- admin/modif_article.php
+++ admin/modif_article.php
@@ -14,32 +14,31 @@
         <?php include_once('includes/header.php'); ?>
         <?php include_once('includes/menu_admin.php'); ?>
         <?php
-            $id = $_GET['id'];
+            if(isset($_GET['id']))
+            {
+                $id = $_GET['id'];
+                $req = $bdd->prepare('SELECT idArticles, titre, contenu, date_redac, login FROM articles LEFT JOIN comptes ON comptes_idcomptes = idcomptes WHERE idArticles = :id');
+                $req->execute(array(
+                    'id' => $id
+                ));
 
-            $req = $bdd->prepare('SELECT idArticles, titre, contenu, date_redac, login FROM articles LEFT JOIN comptes ON comptes_idcomptes = idcomptes WHERE idArticles = :id');
-            $req->execute(array(
-                'id' => $id
-            ));
+                $article = $req->fetch();
+                $req->closeCursor();
+            }
 
-            $article = $req->fetch();
-            $req->closeCursor();
         ?>
             <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-                    <form class="add_article" action="formulaires/edit_article.php" method="post">
+                <form class="add_article" action="formulaires/edit_article.php" method="post">
                     <label for="title">Titre</label>
-                    <br>
-                    <input type="text" id="title" name="title" value="<?php echo $article['titre']; ?>">
-                    <br>
+                    <input type="text" id="title" name="title" value="<?php if(isset($_GET['id'])){echo $article['titre'];} ?>"><br>
+                    <label for="id">ID article</label>
+                    <input type="text" name="id" value="<?php if(isset($_GET['id'])) {echo $id;} ?>"><br>
                     <div>
-                        <p>
-                            <label for="article">Article</label>
-                        </p>
-
+                        <p><label for="article">Article</label></p>
                     </div>
-                    <textarea name='article' id="textarea" cols="150" rows="10"><?php echo $article['contenu']; ?></textarea>
-                    <input type="text" name="id" value="<?php echo $id ?>" hidden>
-                    <br>
+                    <textarea name='article' id="textarea" cols="150" rows="10"><?php if(isset($_GET['id'])){echo $article['contenu'];} ?></textarea><br>
                     <input type="submit" name="valider" value="Modifier l'article">
+                </form>
             </div>
 
         <?php include_once('includes/scripts.php'); ?>
